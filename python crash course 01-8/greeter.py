55ff013014ca93529d49562877e4d5061092ddb2
awoka333 <na23okzw.htre@gmail.com>
2021-02-26 20:12:11 +0900

    [add]戻り値 exercise

--- python crash course 01-8/greeter.py
+++ python crash course 01-8/greeter.py
@@ -1,5 +1,18 @@
-def greet_user(username):
-	"""シンプルなあいさつメッセージを出力する"""
-	print(f"こんにちは{username.title()}！")
+def get_formatted_name(first_name, last_name, middle_name=""):
+	"""フォーマットされたフルネームを返す"""
+	full_name = f"{first_name} {last_name}"
+	return full_name.title()
 
-greet_user('jesse')
+# これは無限ループです
+while True:
+	print("\nあなたの名前を教えて下さい。")
+	print("('q'を入力すると終了します。)")
+	l_name = input("姓を入力してください：")
+	if l_name == 'q':
+		break
+	f_name = input("名を入力してください：")
+	if f_name == 'q':
+		break
+
+	formatted_name = get_formatted_name(f_name, l_name)
+	print(f"こんにちは{formatted_name}！")
